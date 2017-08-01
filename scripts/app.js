ffd8a8bb1b00349e77f8289b7c776257fa5140ed
jrzollin <jrzollin@yahoo.com>
2017-08-01 14:14:39 -0700

    finshed file directory and sorted files

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var projects = [];
+
 function Project(name, image, description){
   this.name = name;
   this.image = image;
   this.description = description;
+  this.addToProjects();
+}
+
+Project.prototype.addToProjects = function(){
+  projects.push(this);
 }
