74462e39d13e5a7f76f7f9c8616bd61e7136229a
jrzollin <jrzollin@yahoo.com>
2017-08-01 20:07:10 -0700

    added hide and show menu functionality

--- scripts/app.js
+++ scripts/app.js
@@ -12,3 +12,26 @@ function Project(name, image, description){
 Project.prototype.addToProjects = function(){
   projects.push(this);
 }
+
+var menuFunctions = {};
+
+menuFunctions.showMenu = function(){
+  $('#nav').on('click', '.show-menu', function(e){
+    e.preventDefault();
+    $('ul').fadeIn();
+    $('.show-menu').text('Hide Menu').attr('class', 'hide-menu');
+  });
+}
+
+menuFunctions.hideMenu = function(){
+  $('#nav').on('click', '.hide-menu' ,function(e){
+    e.preventDefault();
+    $('ul').fadeOut();
+    $('.hide-menu').text('Show Menu').attr('class', 'show-menu');
+  });
+}
+
+$(document).ready(function(){
+  menuFunctions.showMenu();
+  menuFunctions.hideMenu();
+});
