c3db83379a6da35f5a947baab16d250e58073952
jrzollin <jrzollin@yahoo.com>
2017-09-04 03:28:00 -0700

    reworked to MVC files

---
+++ public/scripts/controller/routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var app = app || {};
+
+page('/', app.home.init);
+
+page();
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.get('/*', function(there, backAgain) {
+  backAgain.sendFile('index.html', {root: './public'});
+});
+
 app.listen(PORT, function(){
   console.log(`Port Number: ${PORT}`);
 });
